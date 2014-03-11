9451ef27777163c01a14471ec933b50c3b2713a5
Antony Denyer <antonydenyer@gmail.com>
2014-03-11 19:03:45 +0000

    see readme comments

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,35 +1,35 @@
 class Airport
 
-	DEFAULT_CAPACITY = 20
+  DEFAULT_CAPACITY = 20
 
-	def planes
-		@planes ||= []
-	end
+  def planes
+    @planes ||= []
+  end
 
-	def empty?
-		planes.count == 0
-	end
+  def empty?
+    planes.count == 0
+  end
 
-	def full?
-		planes.count == capacity
-	end
+  def full?
+    planes.count == capacity
+  end
 
-	def capacity
-		@capacity ||= DEFAULT_CAPACITY
-	end
+  def capacity
+    @capacity ||= DEFAULT_CAPACITY
+  end
 
-	def land_plane(plane)
-		raise "The Airport is full" if full?
-		raise "You cannot land in stormy weather" if weather == "stormy"
-		planes << plane
-		plane.land
-	end
+  def land_plane(plane)
+    raise "The Airport is full" if full?
+    raise "You cannot land in stormy weather" if weather == "stormy"
+    planes << plane
+    plane.land
+  end
 
-	def take_off_plane(plane)
-		raise "The Airport is empty" if empty?
-		raise "You cannot take off in stormy weather" if weather == "stormy"
-		planes.delete(plane)
-		plane.take_off
-	end
+  def take_off_plane(plane)
+    raise "The Airport is empty" if empty?
+    raise "You cannot take off in stormy weather" if weather == "stormy"
+    planes.delete(plane)
+    plane.take_off
+  end
 
 end
